d2df8b7c93a0f2874ccd4cc49da83d63ba81f6a3
Annika Faucon <annika.b.faucon@vanderbilt.edu>
2020-10-20 21:43:39 -0500

    Add timer functionality

--- src/PRS.jl
+++ src/PRS.jl
@@ -4,7 +4,7 @@ module PRS
 
 using CSV
 using DataFrames, DataFramesMeta
-using Distributions, Statistics, Random, LinearAlgebra, Printf
+using Dates, Distributions, Statistics, Random, LinearAlgebra, Printf
 using HDF5
 
 include("parse_genet.jl")
@@ -75,6 +75,9 @@ settings = ArgParseSettings()
     "--quiet"
         help = "Disable all unnecessary printing"
         action = :store_true
+    "--hostsfile"
+        help = "Hostsfile to use for parallel processing"
+        default = nothing
 end
 
 function main()
@@ -86,13 +89,15 @@ function main()
 
     ref_dir = opts["ref_dir"]
     verbose && @info "Parsing reference file: $ref_dir/snpinfo_1kg_hm3"
+    t = now()
     ref_df = parse_ref(ref_dir * "/snpinfo_1kg_hm3", chroms)
-    verbose && @info "$(nrow(ref_df)) SNPs in reference file"
+    verbose && @info "$(nrow(ref_df)) SNPs in reference file ($(round(now()-t, Dates.Second)))"
 
     bim_prefix = opts["bim_prefix"]
     verbose && @info "Parsing BIM file: $(bim_prefix*".bim")"
+    t = now()
     vld_df = parse_bim(bim_prefix, chroms)
-    verbose && @info "$(nrow(vld_df)) SNPs in BIM file"
+    verbose && @info "$(nrow(vld_df)) SNPs in BIM file ($(round(now()-t, Dates.Second)))"
 
     for chrom in chroms
         _main(chrom, ref_df, vld_df, opts; verbose=verbose)
@@ -101,15 +106,19 @@ end
 function _main(chrom, ref_df, vld_df, opts; verbose=false)
     sst_file = opts["sst_file"]
     verbose && @info "(Chromosome $chrom) Parsing summary statistics file: $sst_file"
+    t = now()
     sst_df = parse_sumstats(ref_df[ref_df.CHR .== chrom,:], vld_df[vld_df.CHR .== chrom,:], sst_file, opts["n_gwas"]; verbose=verbose, missingstring=opts["sst_missing"])
-    verbose && @info "(Chromosome $chrom) $(nrow(sst_df)) SNPs in summary statistics file"
+    verbose && @info "(Chromosome $chrom) $(nrow(sst_df)) SNPs in summary statistics file ($(round(now()-t, Dates.Second)))"
 
     verbose && @info "(Chromosome $chrom) Parsing reference LD"
+    t = now()
     ld_blk, blk_size = parse_ldblk(opts["ref_dir"], sst_df, chrom)
+    verbose && @info "(Chromosome $chrom) Completed parsing reference LD ($(round(now()-t, Dates.Second)))"
 
     verbose && @info "(Chromosome $chrom) Initiating MCMC"
+    t = now()
     beta_est = mcmc(opts["a"], opts["b"], opts["phi"], sst_df, opts["n_gwas"], ld_blk, blk_size, opts["n_iter"], opts["n_burnin"], opts["thin"], chrom, opts["beta_std"], opts["seed"]; verbose=verbose)
-    verbose && @info "(Chromosome $chrom) Completed MCMC"
+    verbose && @info "(Chromosome $chrom) Completed MCMC ($(round(now()-t, Dates.Second)))"
 
     verbose && @info "(Chromosome $chrom) Writing posterior effect sizes"
     eff_file = if opts["out_path"] === nothing
@@ -120,11 +129,13 @@ function _main(chrom, ref_df, vld_df, opts; verbose=false)
     else
         opts["out_path"]
     end
+    t = now()
     out_df = sst_df[:, [:SNP, :BP, :A1, :A2]]
     out_df[!, :CHR] .= chrom
     out_df.BETA = map(b->@sprintf("%.6e", b), beta_est)
     out_df = select(out_df, [:CHR, :SNP, :BP, :A1, :A2, :BETA])
     CSV.write(eff_file, out_df; header=opts["out_header"], delim=opts["out_delim"])
+    verbose && @info "(Chromosome $chrom) finished writing posterior effect sizes ($(round(now()-t, Dates.Second)))"
 end
 
 end # module
